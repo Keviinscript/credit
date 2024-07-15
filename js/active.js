22525957c297e18a119396a8fe47bdf1b243be64
ikechukwu kevin-chika <ikechukwukevin725@gmail.com>
2024-07-15 15:41:44 +0100

    update

--- js/active.js
+++ js/active.js
@@ -22,11 +22,11 @@
     welcomeSlide.owlCarousel({
       items: 1,
       loop: true,
-      nav: true,
-      navText: [
-        '<i class="fa fa-angle-left"></i>',
-        '<i class="fa fa-angle-right"></i>',
-      ],
+      nav: false,
+      // navText: [
+      //   '<i class="fa fa-angle-left"></i>',
+      //   '<i class="fa fa-angle-right"></i>',
+      // ],
       dots: true,
       autoplay: true,
       autoplayTimeout: 10000,
